1b567fe4432b89fa3712c71c36d79b96f001f239
HusnulM <husnulmub@gmail.com>
2023-04-01 14:48:43 +0700

    udpate

--- app/Http/Controllers/Transaksi/PrintDocumentController.php
+++ app/Http/Controllers/Transaksi/PrintDocumentController.php
@@ -165,9 +165,9 @@ class PrintDocumentController extends Controller
         $pohdr = DB::table('v_rpo')->where('id', $id)->first();
         $podtl = DB::table('t_po02')->where('ponum', $pohdr->ponum)->get();
 
+        $pdf = PDF::loadview('transaksi.po.formpo', ['pohdr' => $pohdr, 'poitem' => $podtl]);
         $pdf->setOptions(['isRemoteEnabled' => true]);
         $pdf->setProtocol($_SERVER['DOCUMENT_ROOT']);
-        $pdf = PDF::loadview('transaksi.po.formpo', ['pohdr' => $pohdr, 'poitem' => $podtl]);
         // $pdf = PDF::loadview('transaksi.po.printpo', ['pohdr' => $pohdr, 'poitem' => $podtl]);
         return $pdf->stream();
     }
