72a3d965c29849d16dc77d3f99550514ce9f4ea7
HusnulM <husnulmub@gmail.com>
2023-04-01 14:53:01 +0700

    udpate

--- resources/views/transaksi/po/formpo.blade.php
+++ resources/views/transaksi/po/formpo.blade.php
@@ -215,7 +215,7 @@
                     <img src="{{ public_path(Auth::user()->s_signfile ?? '') }}" class="img-thumbnail" alt="E-sign" style="width:100px; height:100px;">                    
                     @else
                     
-                    <img src="/{{ Auth::user()->s_signfile }}" class="img-thumbnail" alt="E-sign" style="width:100px; height:100px;">
+                    <img src="asset('{{ Auth::user()->s_signfile }}')" class="img-thumbnail" alt="E-sign" style="width:100px; height:100px;">
                     @endif
                     @endif
                 </td>
