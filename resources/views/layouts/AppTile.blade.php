c3671f4bf825d93d19ac29aba816aca2bd4db81a
Husnul Mubarraq <Husnul.Mubarraq@corp.imip.co.id>
2023-06-09 13:02:08 +0700

    update

--- resources/views/layouts/AppTile.blade.php
+++ resources/views/layouts/AppTile.blade.php
@@ -5,8 +5,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
   <title>@yield('title')</title>
-
-  <link rel="icon" type="image/x-icon" href="{{ asset('/assets/img/logo.png') }}">
+  <link rel="icon" type="image/x-icon" href="{{ asset(getCompanyLogo()) }}">
+  <!-- <link rel="icon" type="image/x-icon" href="{{ asset('/assets/img/logo.png') }}"> -->
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
