e82f7d53af4875b0bc05279a6d8052a599460f56
HusnulM <husnulmub@gmail.com>
2023-10-19 09:45:14 +0700

    update

--- app/Http/Controllers/Transaksi/ApprovePurchaseRequestController.php
+++ app/Http/Controllers/Transaksi/ApprovePurchaseRequestController.php
@@ -378,7 +378,7 @@ class ApprovePurchaseRequestController extends Controller
                             'is_active'         => $po->is_active,
                             'createdon'         => $po->createdon,
                             'approval_remark'   => $po->approval_remark,
-                            'approval_date'     => $po->approval_date,
+                            'approval_date'     => $po->approval_date ?? null,
                             'approved_by'       => $po->approved_by
                         );
                         array_push($insertApproval, $approvals);
