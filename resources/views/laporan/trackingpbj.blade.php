1af955a8fe0fc593a96bf8ce1ac75c5fae3b3e9f
HusnulM <husnulmub@gmail.com>
2023-10-24 21:13:22 +0700

    udpate report tracking PBJ

--- resources/views/laporan/trackingpbj.blade.php
+++ resources/views/laporan/trackingpbj.blade.php
@@ -5,7 +5,7 @@
 @section('additional-css')
 @endsection
 
-@section('content')        
+@section('content')
 <div class="container-fluid">
     <div class="row">
         <div class="col-lg-12">
@@ -49,10 +49,10 @@
                                     </div>
                                     <div class="col-lg-3" style="text-align:center;">
                                         <br>
-                                        <button type="button" class="btn btn-default mt-2 btn-search"> 
+                                        <button type="button" class="btn btn-default mt-2 btn-search">
                                             <i class="fa fa-search"></i> Filter
                                         </button>
-                                        <!-- <button type="submit" class="btn btn-success mt-2 btn-export"> 
+                                        <!-- <button type="submit" class="btn btn-success mt-2 btn-export">
                                             <i class="fa fa-download"></i> Export Data
                                         </button> -->
                                     </div>
@@ -68,6 +68,7 @@
                                     <th>No</th>
                                     <th>Tanggal PBJ</th>
                                     <th>Nomor PBJ</th>
+                                    <th>Nama Item</th>
                                     <th>Status PBJ</th>
                                     <th>Next Approver</th>
                                     <th>Tanggal PR</th>
@@ -81,7 +82,7 @@
                                     <th>Status GR</th>
                                 </thead>
                                 <tbody>
-        
+
                                 </tbody>
                             </table>
                         </div>
@@ -147,7 +148,7 @@
                     { "data": null,"sortable": false, "searchable": false,
                         render: function (data, type, row, meta) {
                             return meta.row + meta.settings._iDisplayStart + 1;
-                        }  
+                        }
                     },
                     {data: "tgl_pbj", className: 'uid',
                         // render: function (data, type, row){
@@ -155,6 +156,7 @@
                         // }
                     },
                     {data: "pbjnumber", className: 'uid'},
+                    {data: "description", className: 'uid'},
                     {data: "pbjstatus"},
                     {data: "pbjnextapproved"},
                     {data: "prdate",
@@ -178,10 +180,10 @@
                     },
                     {data: "docnum"},
                     {data: "grstatus"}
-                ]  
+                ]
             });
         }
-        
+
 
         $('.inputNumber').on('change', function(){
             this.value = formatRupiah(this.value,'');
@@ -193,14 +195,14 @@
             sisa     		  = split[0].length % 3,
             rupiah     		  = split[0].substr(0, sisa),
             ribuan     		  = split[0].substr(sisa).match(/\d{3}/gi);
-        
+
             if(ribuan){
                 separator = sisa ? ',' : '';
                 rupiah += separator + ribuan.join(',');
             }
-        
+
             rupiah = split[1] != undefined ? rupiah + '.' + split[1] : rupiah;
-            return prefix == undefined ? rupiah : (rupiah ? '' + rupiah : '');            
+            return prefix == undefined ? rupiah : (rupiah ? '' + rupiah : '');
         }
     });
 </script>
